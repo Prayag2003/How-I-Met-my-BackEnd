165aee68bb8a44245bb3ca831e04791b5a506644
Prayag <prayagbhatt2003@gmail.com>
2023-04-02 22:13:21 +0530

    Bcrypt and JWT

--- Registration_Form/src/app.js
+++ Registration_Form/src/app.js
@@ -1,11 +1,18 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
 const path = require("path");
 const hbs = require("hbs");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+
 const port = process.env.PORT || 3000;
 require("./db/connection")
 
 const Register = require("./models/registers");
+const { log } = require("console");
 const staticPath = path.join(__dirname, "../public");
 // console.log(staticPath); 
 const templatePath = path.join(__dirname, "../templates/views");
@@ -16,6 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false })) // Important to view data after registration
 // Setting up the Default Template Engine as HBS
 app.use(express.static(staticPath));
+app.use(cookieParser()); // since it acts as a middleware
 app.set("view engine", "hbs");
 app.set("views", templatePath);
 //registering the Partials
@@ -26,8 +34,62 @@ app.get("/", (req, res) => {
     res.render("index");
 })
 
+app.get("/secret", (req, res) => {
+
+    console.log(`My Cookie Token :- ${req.cookies.jwt}`);
+    res.render("secret");
+})
+
 app.get("/register", (req, res) => {
     res.render("register");
+})
+
+app.get("/login", (req, res) => {
+    res.render("login");
+})
+
+app.post("/login", async (req, res) => {
+    try {
+        const email = req.body.email;
+        const password = req.body.pass;
+        // console.log(`Email is ${email} and Password is ${password}`);
+
+        // Since key value pair are same , we can directly write email , all thanks to Object destructuring
+
+        const userEmail = await Register.findOne({ email: email })
+        // res.send(userEmail.password);
+        // console.log(userEmail);
+
+        const isMatch = await bcrypt.compare(password, userEmail.password);
+        // console.log(isMatch);
+
+        // MiddleWare
+        const token = await userEmail.generateAuthToken();
+        console.log("Post Login Token : ", token);
+
+        res.cookie("jwt", token, {
+            // Cookie expires after 10 seconds
+            expires: new Date(Date.now() + 500000),
+            // JS cannot change/misuse cookie
+            httpOnly: true,
+            // secure: true Works only for Secured Connections (https)
+        });
+
+
+        // Accessing the token stored in the cookie by using cookie-parser ( acts as a Handlebar )
+
+
+
+        if (isMatch) {
+            res.status(201).render("index");
+        } else {
+            res.render("404error");
+        }
+
+    } catch (e) {
+        // Never write invalid email or password ( dont give away the details )
+        res.status(400).send(" Kya Hacker banega Tu , Details toh sahi daal !" + e);
+    }
 })
 
 app.post("/register", async (req, res) => {
@@ -36,8 +98,9 @@ app.post("/register", async (req, res) => {
         // res.send(req.body.fname);
         const password = req.body.pass;
         const cpassword = req.body.cpass;
+
         if (password === cpassword) {
-            const register = new Register({
+            const registerkardiya = new Register({
                 firstname: req.body.fname,
                 lastname: req.body.lname,
                 age: req.body.age,
@@ -47,23 +110,57 @@ app.post("/register", async (req, res) => {
                 password: req.body.pass,
                 confirmpassword: req.body.cpass,
             })
-            const registered = await register.save();
+
+            // Hashing the Password ( Middleware ) --> go to models/registers.js
+            // JWT Auth Middleware
+            const token = await registerkardiya.generateAuthToken();
+            console.log(token);
+
+            // cookie sets the Cookie name to value
+            // res.cookie( name , value , [options] )
+
+            res.cookie("jwt", token, {
+                // Cookie expires after 30 seconds
+                expires: new Date(Date.now() + 500000),
+                // JS cannot change/misuse cookie
+                httpOnly: true
+            })
+
+            const registered = await registerkardiya.save();
+            console.log(registered);
+
             res.status(201).render("index");
 
         } else {
             res.send("Passwords are not matching");
         }
-
     }
     catch (e) {
         res.status(400).send(e);
+        console.log("Error aaya BC");
     }
 })
 
 // When Handlebars Code fails to work  ( BackUp )
+
 app.get("/", (req, res) => {
     res.send("Hello ! ... There is Some Technical Error ");
 })
+
+// const createToken = async () => {
+//     // passing some unique data from the database like ID and passing the secret key (should be atleast 32 letters long).
+//     // Server doesn't the states of the token ( Stateless )
+//     const token = await jwt.sign({ _id: "64291fe1ce0f2660d53f92dc" }, process.env.SECRET_KEY_2 , {
+//         expiresIn: "1 minutes"
+//     });
+//     console.log(token);
+
+//     // Verification
+//     const isVerified = await jwt.verify(token, SECRET_KEY_2);
+//     console.log(isVerified);
+// }
+
+// createToken();
 
 app.listen(port, () => {
     console.log(`Servering is Running on the Port ${port}`);
--- Registration_Form/src/models/registers.js
+++ Registration_Form/src/models/registers.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const studentSchema = new mongoose.Schema({
     firstname: {
@@ -35,8 +37,53 @@ const studentSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
+    tokens: [{
+        token: {
+            type: String,
+            required: true,
+        }
+    }]
 })
 
+// Methods are called when we work with Instance,
+// Use Statics when interacting with the Model Directly
+
+// Generating Tokens
+studentSchema.methods.generateAuthToken = async function () {
+    try {
+
+        const generatedToken = await jwt.sign({ _id: this._id.toString() }, process.env.SECRET_KEY )
+        // console.log(generatedToken);
+        // Adding the 
+
+        
+        this.tokens = this.tokens.concat({ token: generatedToken });
+
+        // adding to the Database
+        await this.save();
+
+        return generatedToken;
+
+    } catch (e) {
+        res.send(e);
+        console.log("Error is ", e);
+    }
+}
+
+// Before Schema Modelling , ( takes event , Hashing function)
+// Converting Password into Hash
+studentSchema.pre("save", async function (next) {
+    // const Hashpassword = await bcrypt.hash(password , 10);
+    if (this.isModified("password")) {
+        // console.log(`The Current Password is ${this.password}`);
+        this.password = await bcrypt.hash(this.password, 10);
+        // console.log(`The Current Password is ${this.password}`);
+
+        this.confirmpassword = await bcrypt.hash(this.password, 10);
+
+        next();
+    }
+})
 
 const Register = new mongoose.model("Register", studentSchema);
 
